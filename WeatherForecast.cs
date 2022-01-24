6dae5b7cc73beac674341727831cf88b37251982
Eswar Voleti <eswar.voleti@flexoffers.com>
2022-01-24 22:42:25 +0530

    1. Intern commit test

--- WeatherForecast.cs
+++ WeatherForecast.cs
@@ -4,7 +4,7 @@ namespace MyBooks
 {
     public class WeatherForecast
     {
-        public DateTime Date { get; set; }
+        public DateTime Date { get; set; } 
 
         public int TemperatureC { get; set; }
 
